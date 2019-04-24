9937e6106731fb8fac8aad885a25c2a6020f4a8b
Lorenzo <lgdandrea4@gmail.com>
2019-04-24 16:46:56 -0500

    setup tags and id's on HTML and put together open functions that I believe I will need for the actual game

---
+++ assets/javascript/game.js
@@ -0,0 +1,36 @@
+const CHARACTERS = [
+    {name: Bell},
+    {name: Gaston},
+    {name: Ariel},
+    {name: Abu},
+    {name: Aladdin},
+    {name: Jasmine},
+    {name: Mickey},
+    {name: Minnie},
+    {name: Donald},
+    {name: Daisy},
+    {name: Goofy},
+    {name: Pluto},
+    {name: Simba},
+    {name: Mufasa},
+    {name: Nala},
+    {name: Scar},
+    {name: Rifiki},
+    {name: Elsa},
+    {name: Anna},
+];
+
+
+function startGame () {
+    var x = document.getElementById("start_game");
+    }
+
+function switchName (list_of_names) {
+    var 
+}
+
+function winScore () {
+
+}
+
+switchName(CHARACTERS); 
